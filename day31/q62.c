307c28ea386c53cc65a36cb99d1dc6cd73111f0b
Aaniya Verma <aaniyaverma28@gmail.com>
2025-12-02 18:07:23 +0000

    done

---
+++ day31/q62.c
@@ -0,0 +1,35 @@
+//Reverse an array without taking extra space.
+#include <stdio.h>
+
+int main() {
+    int n, i, temp;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse logic (two-pointer method)
+    int start = 0, end = n - 1;
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
